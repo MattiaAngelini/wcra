719c3ff0cd616afccd2615c3a8973e7b23ca7675
MattiaAngelini <mattiaangelini1993@gmail.com>
2024-07-22 15:40:34 +0200

    feat: add scroll behavior on routes links

--- src/router.js
+++ src/router.js
@@ -5,32 +5,36 @@ import AppSchedule from './pages/AppSchedule.vue';
 import AppIstruttori from './pages/AppIstruttori.vue';
 import AppSedi from './pages/AppSedi.vue';
 
+const routes = [
+    {
+        path: '/',
+        name: 'home',
+        component: AppHome
+    },
+    {
+        path: '/orari-corsi',
+        name: 'schedule',
+        component: AppSchedule
+    },
+    {
+        path: '/istruttori',
+        name: 'istruttori',
+        component: AppIstruttori
+    },
+    {
+        path: '/sedi',
+        name: 'sedi',
+        component: AppSedi
+    }
+];
+
 const router = createRouter({	
     history: createWebHistory(),
-    routes: [
-        {
-            path: '/',
-            name: 'home',
-            component: AppHome
-        },
-        {
-            path: '/orari-corsi',
-            name: 'schedule',
-            component: AppSchedule
-        },
-
-        {
-            path: '/istruttori',
-            name: 'istruttori',
-            component: AppIstruttori
-        },
-       
-        {
-            path: '/sedi',
-            name: 'sedi',
-            component: AppSedi
-        }
-    ]
+    routes,
+    scrollBehavior() {
+            return { left: 0, top: 0 };
+        
+    }
 });
 
 export { router };
